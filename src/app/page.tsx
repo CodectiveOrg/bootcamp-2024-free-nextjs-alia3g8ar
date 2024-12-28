34badb28db2edb66b7fe0c8b356324f6a0206ce8
alia3g8ar <alia3g8ar@gmail.com>
2024-12-28 19:14:21 +0330

    [REMOVE] unnecessary code and files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,5 @@ type Post = {
 };
 
 export default async function Home() {
-  const res = await fetch("https://jsonplaceholder.org/posts", {
-    cache: "no-store",
-  });
-
-  const posts: Post[] = await res.json();
-
-  return (
-    <>
-      <h1>سلام , دنیا !!</h1>
-
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-    </>
-  );
+  return <h1>سلام , دنیا !!</h1>;
 }
