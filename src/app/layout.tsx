9b2328d28a538991c59cddd39e12c6cd27049576
alia3g8ar <alia3g8ar@gmail.com>
2024-12-28 16:36:37 +0330

    [CL/CD] [ADD] lint.yml

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
-import type { Metadata } from 'next';
-import { Vazirmatn } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Vazirmatn } from "next/font/google";
+import "./globals.css";
 
 const vazirmatn = Vazirmatn({
-  subsets: ['latin', 'arabic'],
-  display: 'swap',
+  subsets: ["latin", "arabic"],
+  display: "swap",
 });
 export const metadata: Metadata = {
-  title: 'سلامتِ کندو',
-  description: 'پلتفرم جامع جستجوی دکتر و رزرو نوبت آنلاین',
+  title: "سلامتِ کندو",
+  description: "پلتفرم جامع جستجوی دکتر و رزرو نوبت آنلاین",
 };
 
 export default function RootLayout({
