d8d19b8010b10c9eebe1511b27cfd6d0a95b797b
alia3g8ar <127012198+alia3g8ar@users.noreply.github.com>
2024-12-31 16:17:09 +0330

    Lesson 2  (#3)
    * [CL/CD] [ADD] lint.yml
    
    * [ADD] postcss nesting
    
    * [ADD] css rest
    
    * [ADD] css variables
    
    * [ADD] dark mode
    
    * [REMOVE] unnecessary code and files
    
    * [ADD] header
    
    * [ADD] footer
    
    * [Update] home page

--- src/app/about/page.tsx
+++
@@ -1,4 +0,0 @@
-import { ReactElement } from 'react';
-export default function page(): ReactElement {
-  return <div>about we</div>;
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,38 @@
-import type { Metadata } from 'next';
-import { Vazirmatn } from 'next/font/google';
-import './globals.css';
+import { ReactElement } from "react";
+
+import type { Metadata } from "next";
+import { Vazirmatn } from "next/font/google";
+
+import HeaderComponent from "@/components/header/header.component";
+import FooterComponent from "@/components/footer/footer.component";
+
+import "./globals.css";
 
 const vazirmatn = Vazirmatn({
-  subsets: ['latin', 'arabic'],
-  display: 'swap',
+  subsets: ["latin", "arabic"],
+  display: "swap",
 });
+
 export const metadata: Metadata = {
-  title: 'سلامتِ کندو',
-  description: 'پلتفرم جامع جستجوی دکتر و رزرو نوبت آنلاین',
+  title: "دکتر من",
+  description: "پلتفرم جامع جستجوی دکتر و رزرو نوبت آنلاین",
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>): ReactElement {
   return (
     <html lang="fa" dir="rtl" className={vazirmatn.className}>
-      <body>{children}</body>
+      <body>
+        <HeaderComponent />
+        <main>{children}</main>
+        <p className="tagline">
+          نوبت دهی پزشکی، سامانه نوبت دهی اینترنتی بیمارستان و پزشکان
+        </p>
+        <FooterComponent />
+      </body>
     </html>
   );
 }
